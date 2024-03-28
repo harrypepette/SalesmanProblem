7020a7d79a455a6ed6e6167e4421be00e1dd6d3c
harrypepette <sattiaharish@gmail.com>
2024-03-28 15:07:47 +0100

    test

---
+++ geneticAlgo.py
@@ -0,0 +1,7 @@
+import random
+
+class Genetic:
+    def __init__(self, population, population_size=0):
+        self.population = population
+        self.size= population_size
+        pass
---
+++ main.py
@@ -0,0 +1,1 @@
+import SalesmanProblem.geneticAlgo as ga
